03ae2d7bdea316f4cd1181c5889c797084bb2292
Eden Mesfun <emesfun@gmail.com>
2018-01-11 16:38:04 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
-  def name = (dog_name)
+  def name=(dog_name)
     @name = dog_name
   end
   def name
     @name
   end
-  def breed = (breed_name)
+  def breed=(breed_name)
     @breed = breed_name
   end
   def breed
